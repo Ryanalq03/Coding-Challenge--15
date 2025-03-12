137f1736aed57608da1c18d37b112660a1c1475c
Ryan Alqasemi <alqasemir@usf.edu>
2025-03-12 16:44:47 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,7 @@
+//Coding Challenge #15
+
+//Selects the riskDashboard container
+const riskDashboard = document.getElementById('riskDashboard');
+
+//Reads the console message
+console.log("Risk Dashboard Loaded");
