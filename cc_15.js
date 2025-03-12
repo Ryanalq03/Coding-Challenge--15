85f11df391a340a5c9cb4652de3ba60be1ef115e
Ryan Alqasemi <alqasemir@usf.edu>
2025-03-12 17:19:52 -0400

    Task 2 - Adding Risk Items

--- cc_15.js
+++ cc_15.js
@@ -5,3 +5,33 @@ const riskDashboard = document.getElementById('riskDashboard');
 
 //Reads the console message
 console.log("Risk Dashboard Loaded");
+
+//Task 2: Adding Dynamic Risk Items
+
+//Added addRiskItem function 
+function addRiskItem(riskName, riskLevel, department){
+    //Created a new risk card element
+    const riskCard = document.createElement('div');
+    riskCard.className = 'riskCard';
+
+    //Created element to display risk details
+    const riskInfo = document.createElement('p');
+    riskInfo.textContent = `Risk: ${riskName} | Level: ${riskLevel} | Department: ${department}`;
+    riskCard.appendChild(riskInfo);
+}
+
+//Added form submission to add new risk items
+const riskForm = document.getElementById('riskForm');
+//Adding event listener
+riskForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    //Gets values from the form inputs
+    const riskName = document.getElementById('riskName').value;
+    const riskLevel = document.getElementById('riskLevel').Value;
+    const riskDepartment = document.getElementById('riskDepartment').value;
+
+    addRiskItem(riskName, riskLevel, riskDepartment);
+    riskForm.reset();
+
+});
